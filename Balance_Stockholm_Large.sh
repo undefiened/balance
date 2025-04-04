4c656faa0460025ae99685bf65c1b6350b47b2a5
undefiened <undefiened@gmail.com>
2025-04-04 11:55:41 +0900

    fix server logs

--- Balance_Stockholm_Large.sh
+++ Balance_Stockholm_Large.sh
@@ -5,8 +5,8 @@
 #SBATCH --exclusive
 #SBATCH -N 1
 #SBATCH --mail-user=leonid.sedov@liu.se
-#SBATCH -o Balance_Stockholm_Large.out
-#SBATCH -e Balance_Stockholm_Large.err
+#SBATCH -o results/Balance_Stockholm_Large_"$1".out
+#SBATCH -e results/Balance_Stockholm_Large_"$1".err
 #SBATCH --mail-type=ALL
 
 #sleep 100
