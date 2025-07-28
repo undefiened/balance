00a9630e13f362f5e9672600f97f125e5db33dda
undefiened <undefiened@gmail.com>
2025-07-28 17:01:43 +0200

    Update running script for the induced subgraph scenario to be consistent with the entire network

--- Balance_Stockholm_Medium.sh
+++ Balance_Stockholm_Medium.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 #SBATCH -t 24:00:00
 #SBATCH -J Balance_Stockholm_Medium
-#SBATCH -N 1
-#SBATCH --mem=16000
+#SBATCH --mem=300000
 #SBATCH --exclusive
-#SBATCH --mail-user=hardy.hasan@liu.se
+#SBATCH -N 1
+#SBATCH --mail-user=leonid.sedov@liu.se
+#SBATCH -o results/Balance_Stockholm_Medium_$1.out
+#SBATCH -e results/Balance_Stockholm_Medium_$1.err
 #SBATCH --mail-type=ALL
 
 #sleep 100
-module load gurobi/9.0.0-nsc1
-module load Python/3.10.4-env-hpc1-gcc-2022a-eb
+module load gurobi/10.0.0-nsc1
+module load Python/3.10.4-bare-hpc1-gcc-2022a-eb
 source venv/bin/activate
 
-mkdir results/stockholm_medium_"$1"
-mkdir results/stockholm_medium_"$1/models"
-touch results/stockholm_medium_"$1/logs.txt"
+mkdir results/stockholm_medium_const_uncert_"$1"
+mkdir results/stockholm_medium_const_uncert_"$1/models"
+touch results/stockholm_medium_const_uncert_"$1/logs.txt"
+touch results/stockholm_medium_const_uncert_"$1/doc.txt"
 
-python3 main.py --graph_name=stockholm_medium --random_intents --run_name="$1" --num_intents="$1" > results/stockholm_medium_"$1"/logs.txt
+python3 main.py --graph_name=stockholm_medium --random_intents --run_name=const_uncert_"$1" --num_intents="$1" > results/stockholm_medium_const_uncert_"$1"/logs.txt
