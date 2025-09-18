6160efe9a1d20a43051938915bc8ce180bb3f9ed
undefiened <undefiened@gmail.com>
2025-09-18 15:33:06 +0200

    Increased timeout

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ This file contains constant values that can be used across the project files.
 
 # --- Time in seconds an ip model is allowed to do solving. ---
 #MAXIMUM_RUNTIME = 3*60*60
-MAXIMUM_RUNTIME = 71*60*60
+MAXIMUM_RUNTIME = 140*60*60
 # --- If a model reaches this gap percentage, stop further optimization. This is used to speed up solving runtime. ---
 ALLOWED_GAP = 0.005
 IP_TIME_HORIZON_MULTIPLIER = 3
