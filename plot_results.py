499d79c923969f1f82e22100d2ced84336851c4c
undefiened <undefiened@gmail.com>
2025-09-18 14:47:34 +0200

    Changes to plotting

--- plot_results.py
+++ plot_results.py
@@ -36,7 +36,7 @@ def plot_results(dataframe, name):
         [(sum(delays) / len(delays)) / 60 for idx, delays in enumerate(ip_delays)]
     ]
 
-    max_travel_time_extension_percentage = 20
+    max_travel_time_extension_percentage = 50
     greedy_travel_time_extension_percentages = [[round(100 * td / ideal_times[idx][indx], 1)
                                                  for indx, td in enumerate(delays)]
                                                 for idx, delays in enumerate(greedy_delays)]
@@ -57,22 +57,38 @@ def plot_results(dataframe, name):
                                                          ip_percentage_of_drones_with_exceeded_time_extension]
     # print(percentage_of_drones_with_exceeded_time_extension)
 
+    # Calculate percentage of intents with travel time extension exceeding 20 minutes (1200 seconds)
+    max_travel_time_extension_minutes = 20
+    max_travel_time_extension_seconds = max_travel_time_extension_minutes * 60
+    
+    greedy_percentage_exceeding_20min = [
+        round(100 * sum([delay > max_travel_time_extension_seconds for delay in delays]) / len(delays), 1)
+        for delays in greedy_delays]
+    
+    ip_percentage_exceeding_20min = [
+        round(100 * sum([delay > max_travel_time_extension_seconds for delay in delays]) / len(delays), 1)
+        for delays in ip_delays]
+    
+    percentage_exceeding_20min = [greedy_percentage_exceeding_20min, ip_percentage_exceeding_20min]
+
     data = [objectives,
             run_times,
             memory_usages,
             maximum_delayed_drones_delay,
             average_travel_time_extension,
-            percentage_of_drones_with_exceeded_time_extension
+            percentage_of_drones_with_exceeded_time_extension,
+            percentage_exceeding_20min
             ]
 
     greedy_color, ip_color = 'mediumaquamarine', 'violet'
-    x_label = 'Number of intents'
+    x_label = 'Number of flight intents'
     y_labels = ['Total travel time extension (min)', 'Runtime (min)', 'Memory usage (GB)',
                 'Delay of most delayed drone (min)', 'Average travel time extension (min)',
-                f'Percentage of drones with a travel time extension more than {max_travel_time_extension_percentage}%']
-    titles = ['', 'Runtimes of the methods', 'Memory usages of the methods', '', '', '']
+                f'Percentage of drones with a travel time extension more than {max_travel_time_extension_percentage}%',
+                f'Percentage of drones with a travel time extension > {max_travel_time_extension_minutes} min']
+    titles = ['', 'Runtimes of the methods', 'Memory usages of the methods', '', '', '', '']
     plot_names = ['objective', 'runtime', 'memory_usage', 'delay', 'average_time_extension',
-                  'percentage_time_extension']
+                  'percentage_time_extension', 'percentage_exceeding_20min']
     x_axis = dataframe.num_intents.values
     fontsize = 12
     linewidth = 3
